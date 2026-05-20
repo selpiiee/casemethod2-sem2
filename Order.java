a357553a118c4bac492b5438256fc2a065b237de
“selpiiee” <selvynurkhoffifa@gmail.com>
2026-05-20 20:15:14 +0700

    CM 2 - Order Class

---
+++ Order.java
@@ -0,0 +1,11 @@
+public class Order {
+    int OrderCode;
+    String OrderName;
+    int price;
+
+    public Order(int a, String b, int d) {
+        this.OrderCode = a;
+        this.OrderName = b;
+        this.price = d;
+    }
+}
